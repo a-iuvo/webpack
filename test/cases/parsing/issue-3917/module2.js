1e74c5dd28b5b05286190afec728f9abf767fe17
kale-io <alpha@codeyourkale.ai>
2017-11-08 14:58:00 +0200

    Convert Non-strict to strict equality checking Avoid the more obscure equality algorithm used when using == / != and rely on the strict equality checking algorithm employed by === / !===

--- test/cases/parsing/issue-3917/module2.js
+++ test/cases/parsing/issue-3917/module2.js
@@ -1,2 +1,2 @@
-var freeExports = typeof exports == 'object' && exports && !exports.nodeType && exports;
+var freeExports = typeof exports === 'object' && exports && !exports.nodeType && exports;
 export default freeExports;
