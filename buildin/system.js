3310c450f3da20ef79f539f6256c191b4885b7ee
kale-io <alpha@codeyourkale.ai>
2017-11-08 15:32:30 +0200

    function expression => arrow function expression Converts a function expression to the new arrow function syntax. Performs the necessary safety checks.

--- buildin/system.js
+++ buildin/system.js
@@ -1,7 +1,7 @@
 // Provide a "System" global.
 module.exports = {
 	// Make sure import is only used as "System.import"
-	import: function() {
+	import: () => {
 		throw new Error("System.import cannot be used indirectly");
 	}
 };
