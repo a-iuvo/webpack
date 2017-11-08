560ea08893dbc83227706f7480b863fbdbf8df71
kale-io <alpha@codeyourkale.ai>
2017-11-08 15:29:30 +0200

    Prefer using rest params over arguments Use the spread operator to capture a function's arguments instead of using the `arguments` variable

--- examples/harmony/math.js
+++ examples/harmony/math.js
@@ -1,5 +1,5 @@
-export function add() {
-	var sum = 0, i = 0, args = arguments, l = args.length;
+export function add(...params) {
+	var sum = 0, i = 0, args = params, l = args.length;
 	while (i < l) {
 		sum += args[i++];
 	}
